cc6b116e368d53c90982a6f61b4e5b218abe580a
Guillaume Roux <guiguionthenet@free.fr>
2026-02-21 09:28:10 +0100

    Ajout de toutes les propriétés d'un user à TraccarUser

--- types/traccar-types.ts
+++ types/traccar-types.ts
@@ -22,6 +22,29 @@ export interface TraccarUser
     email: string;
 }
 
+
+export interface FullTraccarUser extends TraccarUser
+{
+    phone: string;
+    readonly: boolean;
+    administrator: boolean;
+    map: string;
+    latitude: number;
+    longitude: number;
+    zoom: number;
+    password: string;
+    coordinateFormat: string;
+    disabled: boolean;
+    expirationTime: string;
+    deviceLimit: number;
+    userLimit: number;
+    deviceReadonly: boolean;
+    limitCommands: boolean;
+    fixedEmail: boolean;
+    poiLayer: string;
+    attributes: {};
+}
+
 export interface ApiError
 {
     message?: string;
