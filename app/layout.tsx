948c7a9ce019300514e034af54212bea110af069
Sylvain Buisine <buisine.sylvain@gmail.com>
2026-05-14 18:40:51 +0200

    Mise en place image de fond du header

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,13 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <header className="relative isolate flex h-72 w-full overflow-hidden">
-          <div className="absolute inset-0 bg-[url('/images/paturage.jpeg')] bg-cover bg-center" />
+          <div className="absolute inset-0 bg-[url('/images/paysage.png')] bg-cover bg-center" />
           <div className="relative mx-auto flex w-full max-w-6xl flex-col px-4 pb-5 pt-4 sm:px-8 sm:pb-7 sm:pt-6">
-            <div className="flex items-start gap-2 sm:gap-4">
+            {/* <div className="flex items-start gap-2 sm:gap-4">
               <h1 className="flex-1 text-center text-4xl font-extrabold tracking-[0.2em] text-gray-700 sm:text-5xl rounded-2xl bg-white/80 px-4 py-2">
                 OVIN-TRACK
               </h1>
-            </div>
+            </div> */}
             <HeaderNav />
           </div>
         </header>
