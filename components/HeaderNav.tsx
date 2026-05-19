d0eb49a82d8b69019ae5e607aa9efce0750402e3
Guillaume Roux <guiguionthenet@free.fr>
2026-05-19 20:12:14 +0200

    Bouton Périmètre retiré pour les utilisateurs non admin, sur le modèle du bouton Utilisateurs

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -217,17 +217,19 @@ export default function HeaderNav()
                             Appareils
                         </Link>
                     </li>
-                    <li className="w-full sm:w-auto">
-                        <Link
-                            href="/geofences"
-                            className={baseButtonClassName}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
-                            </svg>
+                    {isAdmin && (
+                        <li className="w-full sm:w-auto">
+                            <Link
+                                href="/geofences"
+                                className={baseButtonClassName}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                                </svg>
 
-                            Perimetres
-                        </Link>
-                    </li>
+                                Perimetres
+                            </Link>
+                        </li>
+                    )}
                     <li className="col-span-2 w-full sm:w-auto">
                         <button
                             type="button"
